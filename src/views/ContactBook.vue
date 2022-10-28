01c7c82d558f6fc3fe033417aacb38f55b82780b
nguyenthtruc <ngtruc1210@gmail.com>
2022-10-28 18:12:12 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -16,7 +16,7 @@
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
-            <i  class="far fa-check-square"></i> Làm mới
+            <i class="fa-solid fa-circle-check"></i> Làm mới
         </button>
         <button class="btn btn-sm btn-success" @click="goToAddContact">
           <i class="fas fa-plus"></i> Thêm mới
@@ -33,6 +33,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link 
+          :to="{
+            name: 'contact.edit',
+            params: {id: activeContact._id},
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh
+          </span>
+        </router-link>
       </div>
     </div>
   </div>
